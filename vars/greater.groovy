d421371f4fc746ae85a0c195876c0192d9464713
PavanGowda <139548290+Pavanhgowda@users.noreply.github.com>
2024-03-11 10:41:20 +0530

    Update greater.groovy

--- vars/greater.groovy
+++ vars/greater.groovy
@@ -2,4 +2,9 @@ def compareNumber(a, b) {
     if (a > b) {
         println "The number ${a} is greater than ${b}"
     }
+    else if(b > a){
+        println "The number ${b} is greater than ${a}"
+    else{
+        println "Both Numbers are equal"
+    }
 }
